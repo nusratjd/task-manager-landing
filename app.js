5247f89044987103cfd2fb5c0f0ef31ff7f6d599
Nusrat Jahan Dipa <nusratjd41@gmail.com>
2026-08-09 06:30:14 -0700

    Initial commit

--- app.js
+++ app.js
@@ -12,4 +12,6 @@ function updateCount() {
 from.addEventListener("submit", (e) => {
     e.preventDefault();
     const li = document.createElement("li");
+    li.innerHTML = `<span>${input.value}</span><button class="del"*</button>`;
+    
 })
