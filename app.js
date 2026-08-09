7f0059a921b1328fb3541a0d80525402b7d08983
Nusrat Jahan Dipa <nusratjd41@gmail.com>
2026-08-09 06:25:05 -0700

    Initial commit

--- app.js
+++ app.js
@@ -6,4 +6,5 @@ const count = document.getElementById("count");
 function updateCount() {
     const total = list.children.length;
     const done = list.querySelectorAll("li.done").length;
+    count.textContent = `${total} tasks . ${done} done`;
 }
