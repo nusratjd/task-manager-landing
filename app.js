1ca57e3665d1787722a14c0e4516dfcb5b6f3a75
Nusrat Jahan Dipa <nusratjd41@gmail.com>
2026-08-09 06:44:05 -0700

    Initial commit

--- app.js
+++ app.js
@@ -26,5 +26,6 @@ list.addEventListener("click", (e) => {
 });
 
 document.getElementById("clear").addEventListener("click", () => {
-    
+    list.querySelectorAll("li.done").forEach(li => li.remove());
+
 })
